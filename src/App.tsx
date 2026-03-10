f8e850fa3c4d3956c946e2c4cadbab16dd7ca3d8
xxnicor <nicoedu123@gmail.com>
2026-03-09 21:43:11 -0300

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -45,7 +45,7 @@ const AnimatedRoutes = () => {
         animate="animate"
         exit="exit"
         transition={{
-          duration: shouldReduceMotion ? 0 : 0.14,
+          duration: shouldReduceMotion ? 0 : 0.8,
           ease: 'easeOut',
         }}
         style={{ width: '100%' }}
