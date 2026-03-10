c35afef2b1077a28a6361378e5942c3439be8ea5
xxnicor <nicoedu123@gmail.com>
2026-03-09 21:44:30 -0300

    upadte transition

--- src/App.tsx
+++ src/App.tsx
@@ -45,7 +45,7 @@ const AnimatedRoutes = () => {
         animate="animate"
         exit="exit"
         transition={{
-          duration: shouldReduceMotion ? 0 : 0.8,
+          duration: shouldReduceMotion ? 0 : 0.10,
           ease: 'easeOut',
         }}
         style={{ width: '100%' }}
